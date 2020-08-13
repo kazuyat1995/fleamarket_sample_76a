98559a0c4e1ec45669237d2d2a381737065936ac
atsushs <66734345+atsushs@users.noreply.github.com>
2020-08-13 22:11:35 +0900

    マイページ追加修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :users, only: [:index, :edit]
-  get 'credit_cards/new'
+  resources :credit_cards, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
