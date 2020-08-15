fc5f62a47ece70e71c37f84a35792b15862baa05
kazuyat1995 <kazuyat1995@gmail.com>
2020-08-15 12:15:29 +0900

    ログイン・新規登録機能　db関連
    # What
    新規登録情報　validates
    db カラム追加
    
    # Why
    dbに指定のフォーマットで保存する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :address, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  with_options presence: true do
+    validates :nickname
+    validates :email,   uniqueness: {case_sensitive: false},
+                        format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    validates :password, length: {minimum: 7}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+    get 'address', to: 'users/registrations#new_address'
+    post 'address', to: 'users/registrations#create_address'
+  end
+  root to: 'items#index'
 end
