b47b9d92e72f13e5058a3040610663734f158c95
dErmAnHub <dtools001@gmail.com>
2020-10-06 23:29:44 +0900

    DB関連

--- db/migrate/20200820063235_create_items.rb
+++ db/migrate/20200820063235_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.string :name, null: false
       t.string :detail, null: false
-      # t.references :category, null:false, foreign_key:true
+      t.references :category, null:false, foreign_key:true
       t.string :brand
       t.integer :condition, null: false
       t.integer :postage, null: false
